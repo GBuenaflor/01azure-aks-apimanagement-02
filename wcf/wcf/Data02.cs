4f92a05dab952b3cb58944d5bc605ca4ed8cf4b7
BUENAFLOR, Gerardo C <63630691+GBuenaflor@users.noreply.github.com>
2020-08-26 07:56:22 +0800

    Add files via upload

---
+++ wcf/wcf/Data02.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "Data02" in both code and config file together.
+    [ServiceContract]
+    public interface Data02
+    {
+        [OperationContract]
+        string[] GetDataWCF();
+       
+    }
+
+}
---
+++ wcf/wcf/Data02.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCF" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select WCF.svc or WCF.svc.cs at the Solution Explorer and start debugging.
+    public class WCF : Data02
+    {
+
+        private string[] DataArray02 = new[]
+        {
+            "Data02-0001", "Data02-0002", "Data02-0003", "Data02-0004", "Data02-0005" , "Data02-0006"
+        };
+         
+        
+        public string[] GetDataWCF()
+        {
+            return DataArray02;
+        }
+
+
+    }
+}
